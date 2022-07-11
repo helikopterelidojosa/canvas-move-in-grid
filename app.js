6bee14a7d8083e1d716dfb1ffc4e7d418985c69a
Ramstain111si <82330389+Ramstain111si@users.noreply.github.com>
2022-07-11 03:08:13 +0300

    chess board, btns

--- app.js
+++ app.js
@@ -1,16 +1,83 @@
-//start myCanvas
+    //start myCanvas
+
 var myCanvas = document.getElementById("myCanvas");
 // Get the 2d context of the canvas
 var ctx = myCanvas.getContext("2d");
+//squareCanvas=400;
+var squareCanvas=400;
 //variables for canvas size 400x400
-var canvasWidth = 400;
-var canvasHeight = 400;
+var canvasWidth = squareCanvas;
+var canvasHeight = squareCanvas;
 //set canvas size
 myCanvas.width = canvasWidth;
 myCanvas.height = canvasHeight;
 //set canvas background color
+function setCanvasBackgroundColor(color){
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+}
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+//location of the square
+let x = 0;
+let y = 0;
 
+
+//selectSquareSize = squareCanvas/8;
+var selectSquareSize = squareCanvas/8;
+// draw a stroke square size of selectSquare
+function selectSquare(x, y, color){ 
+
+    //beginPath();
+    
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 4;
+    ctx.strokeRect(x, y, selectSquareSize, selectSquareSize); 
+    
+}
+
+
+//directions for the square
+function moveSquareUp(){
+    y -= selectSquareSize;
+    
+}
+function moveSquareDown(){
+    y += selectSquareSize;
+}
+function moveSquareLeft(){
+    x -= selectSquareSize;
+}
+function moveSquareRight(){
+    x += selectSquareSize;
+}
+
+// chess board
+function chessBoard(){
+    for(let i = 0; i < 8; i++){
+        for(let j = 0; j < 8; j++){
+            if((i + j) % 2 == 0){
+                ctx.fillStyle = "black";
+            }
+            else{
+                ctx.fillStyle = "white";
+            }
+            ctx.fillRect(i * selectSquareSize, j * selectSquareSize, selectSquareSize, selectSquareSize);
+
+        }
+    }
+}
+
+   
+
+
+
+//load all the functions
+setInterval(loadCanvas, 500);
+
+function loadCanvas(){
+chessBoard();
+    selectSquare(x,y,"red");
+}
 
